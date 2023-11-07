f20665efd145385ebbad2ab9ee144abf5767a7db
23navi <navisureka23@gmail.com>
2023-11-07 22:20:10 +0530

    add require auth middleware

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,11 +2,12 @@ import express, { Request, Response } from 'express';
 import validateResource from '../middlewares/validateResources';
 import { createUserSchema } from '../schemas/user.schema';
 import { registerUser, currentUser } from '../controllers/user.controller';
+import requireAuth from '../middlewares/requireAuth';
 
 const router = express.Router();
 
 router.post('/register', validateResource(createUserSchema), registerUser);
 
-router.get('/me', currentUser);
+router.get('/me', requireAuth, currentUser);
 
 export default router;
