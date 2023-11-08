ba33e140d1d1c1c282667b3b9c33f135de0bb20a
23navi <navisureka23@gmail.com>
2023-11-08 11:19:58 +0530

    NotFound error updateg

--- src/controllers/board.controller.ts
+++ src/controllers/board.controller.ts
@@ -7,10 +7,14 @@ import {
   UpdateBoardByIdInputBody,
   DeleteBoardByIdInputParam,
 } from '../schemas/board.schema';
+import Board from '../models/board.modle';
+import { NotFoundError } from '../errors';
 
 const getAllBoards = async (req: Request, res: Response, next: NextFunction) => {
   // We will get board for currently logged in user, we don't need to take in userId as input
-  return res.send('All Boards for the give user');
+  const { _id: userId } = res.locals.user;
+  const boards = await Board.find({ _id: userId });
+  return res.send(boards);
 };
 
 const getBoardById = async (req: Request<GetBoardByIdInput>, res: Response, next: NextFunction) => {
@@ -19,7 +23,12 @@ const getBoardById = async (req: Request<GetBoardByIdInput>, res: Response, next
 
 const createBoard = async (req: Request<{}, {}, CreateBoardSchemaInput>, res: Response, next: NextFunction) => {
   // When we create a board, we should create 3 column and add that to this board, and owner of the column will be owner of board by default
-  return res.send('New board with title = ' + req.body.title);
+  const board = Board.build({
+    title: req.body.title,
+    user: res.locals.user._id,
+  });
+  await board.save();
+  return res.send(board);
 };
 
 const updateBoard = async (
@@ -27,6 +36,10 @@ const updateBoard = async (
   res: Response,
   next: NextFunction,
 ) => {
+  const board = await Board.findById(req.params.id);
+  if (!board) {
+    throw new NotFoundError('Board not Found');
+  }
   return res.send('Updated board with title = ' + req.body.title + 'and id = ' + req.params.id);
 };
 
